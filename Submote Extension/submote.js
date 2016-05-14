f45f3facf1ed05ac44b1b548cb47b72f385bb387
Petter Kraabøl <petter.zarlach@gmail.com>
2016-05-14 12:14:55 +0200

    Twitch is using https. Extension permissions has been changed to use https.

--- Submote Extension/submote.js
+++ Submote Extension/submote.js
@@ -1,8 +1,9 @@
 function submoteInit()
 {
+    date = new Date();
 	script = document.createElement('script');
 	script.type = 'text/javascript';
-	script.src = "https://rawgit.com/Zarlach/Submote/master/submote.js?"+Math.random();
+	script.src = "https://rawgit.com/Zarlach/Submote/master/submote.js?"+date.getTime();
 	head = document.getElementsByTagName('head')[0];
 	if(head) head.appendChild(script);
 }
